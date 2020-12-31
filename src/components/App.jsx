4ac03624eb7ed9ff9008cf41c3052d80d0e7925e
akhilalekha <akhila160@gmail.com>
2020-12-31 21:43:13 +0530

    add card styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,64 @@
 import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import Card from "./Card";
 const keys = require("../config/keys");
 
 function App() {
 	const [items, setItems] = useState([]);
-	const [search, setSearch] = useState("");
+	const [input, setInput] = useState("");
 	const [query, setQuery] = useState("chicken");
 
-	let URI = `https://api.edamam.com/search?q=${query}&app_id=${keys.appID}&app_key=${keys.appKey}`;
-
 	useEffect(() => {
+		let URI = `https://api.edamam.com/search?q=${query}&app_id=${keys.appID}&app_key=${keys.appKey}`;
+		const getRecipes = async () => {
+			const response = await fetch(URI);
+			const data = await response.json();
+			console.log({ data });
+			setItems(data.hits);
+		};
+		console.log("test");
 		getRecipes();
 	}, [query]);
 
-	const getRecipes = async () => {
-		const response = await fetch(URI);
-		const data = await response.json();
-		console.log(data);
-		setItems(data.hits);
-	};
+	function handleChange(e) {
+		setInput(e.target.value);
+	}
 
-	const handleChange = (e) => {
-		setSearch(e.target.value);
-	};
-
-	const getSearch = (e) => {
+	function getSearch(e) {
 		e.preventDefault();
-		setQuery(search);
-		setSearch("");
-	};
+		setQuery(input);
+		setInput("");
+	}
 	return (
 		<div className="App">
 			<h1>Recipe app</h1>
 			<form className="searchForm" onSubmit={getSearch}>
-				<label htmlFor="searchString">Enter your search here: </label>
+				<label htmlFor="searchString"></label>
 				<input
 					id="searchString"
 					type="search"
-					value={search}
+					value={input}
+					placeholder="Search for a recipe"
 					onChange={handleChange}
 				/>
-				<button type="submit">Search</button>
 			</form>
+			<button className="searchBtn" type="submit" onClick={getSearch}>
+				Search
+			</button>
 			<div className="cardContainer">
 				{items.map((item) => (
 					<Card
-						key={item.recipe.label}
+						key={uuidv4()}
 						image={item.recipe.image}
 						name={item.recipe.label}
 						calories={item.recipe.calories}
+						time={item.recipe.totalTime}
+						dietlabels={item.recipe.dietLabels}
+						url={item.recipe.url}
+						ingredients={item.recipe.ingredients}
 					/>
 				))}
-				{/* <Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card /> */}
 			</div>
 		</div>
 	);
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import "./Card.css";
 
-function Card({ image, name, calories }) {
+function Card({ image, name, calories, time, dietlabels, url, ingredients }) {
 	return (
 		<div className="card">
 			<img className="recipeImage" src={image} alt="" />
 			<div className="recipeDetails">
-				<h4>{name}</h4>
-				<p>{Math.floor(calories)} kcal</p>
+				<button className="detailItem">{name}</button>
+				<p className="detailItem one">{time} min</p>
+				<p className="detailItem two">{Math.floor(calories)} kcal</p>
+				{dietlabels.map((label) =>
+					label ? (
+						<p className="detailItem three" key={label}>
+							{label}
+						</p>
+					) : null
+				)}
+
+				<a
+					className="detailItem recipeBtn"
+					href={url}
+					rel="noopener noreferrer"
+					target="_blank"
+				>
+					Recipe
+				</a>
 			</div>
 		</div>
 	);
